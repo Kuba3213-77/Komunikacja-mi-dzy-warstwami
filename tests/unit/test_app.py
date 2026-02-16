76e66e6c4f132a9df4c24371c7c41b9088d99786
Jakub Lipka <u160_jaklip_waw@technischools.com>
2026-02-16 14:56:39 +0100

    dsadsadsa

--- tests/unit/test_app.py
+++ tests/unit/test_app.py
@@ -10,21 +10,21 @@ class TestApp(unittest.TestCase):
 
     @patch('src.app.controller')
     def test_get_book(self, mock_controller):
-        # Prepare mock data
+     
         expected_book = Book(1, 'Test Book', 'Test Author')
         mock_controller.get_books.return_value = expected_book
 
-        # Make request
+       
         response = self.app.get('/book/1')
         
-        # Assertions
+ 
         self.assertEqual(response.status_code, 200)
         data = json.loads(response.data)
         self.assertEqual(data['id'], 1)
         self.assertEqual(data['title'], 'Test Book')
         self.assertEqual(data['author'], 'Test Author')
         
-        # Verify controller usage
+      
         mock_controller.get_books.assert_called_once_with(1)
 
     @patch('src.app.controller')
